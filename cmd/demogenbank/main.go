dd0d0db1e0f1013daf293c134589856695190fdc
tnaums <tanaumann@outlook.com>
2026-04-19 21:45:07 -0500

    refinements to genbank.go in package dna

--- cmd/demogenbank/main.go
+++ cmd/demogenbank/main.go
@@ -11,6 +11,11 @@ func main() {
 	f, _ := os.Open("sequences/sequence.gb")
 	g := dna.NewGenBank(f)
 
+	// Print GenBank.Sequence in fasta format
+	fmt.Println(g.Sequence)
+
+	// loop through orfs of g.Sequence and print anything
+	// larger than 150 amino acids
 	for _, orf := range g.Sequence.Orfs {
 		if len(orf.AminoAcid) > 150 {
 			fmt.Println(orf)
--- internal/dna/genbank.go
+++ internal/dna/genbank.go
@@ -3,7 +3,7 @@ package dna
 import (
 	"bufio"
 	"bytes"
-	//	"fmt"
+	"fmt"
 	"io"
 	"strings"
 )
@@ -27,6 +27,8 @@ const (
 type GenBank struct {
 	Sequence DNA
 	Features []byte
+	Accession string
+	Definition string
 	state    genBankState
 }
 
@@ -38,26 +40,42 @@ func NewGenBank(r io.Reader) GenBank {
 	features := bytes.Buffer{}
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
+		if strings.HasPrefix(scanner.Text(), "//") {
+			g.state = genbankStateDone
+			sequence := strings.Join(strings.Fields(buf.String()), "")
+			header := fmt.Sprintf("%s|%dbp|%s", g.Accession,  len(sequence), g.Definition)
+			g.Sequence = NewDNAFromSequence(header, sequence)
+		}		
 		if strings.HasPrefix(scanner.Text(), "ORIGIN") {
 			g.state = genbankStateOrigin
 			g.Features = features.Bytes()
 			continue
 		}
-		if strings.HasPrefix(scanner.Text(), "//") {
-			g.state = genbankStateDone
-			sequence := strings.Join(strings.Fields(buf.String()), "")
-			g.Sequence = NewDNAFromSequence("genbank", sequence)
-		}
 		if strings.HasPrefix(scanner.Text(), "FEATURES") {
 			g.state = genbankStateFeatures
+			continue
 		}
+		if strings.HasPrefix(scanner.Text(), "ACCESSION") {
+			a := strings.Fields(scanner.Text())
+			g.Accession = a[1]
+			g.state = genbankStateAccession
+			continue
+		}
+		if strings.HasPrefix(scanner.Text(), "DEFINITION") {
+			d := strings.TrimSpace(strings.TrimPrefix(scanner.Text(), "DEFINITION"))
+			g.Definition = d
+			g.state = genbankStateDefinition
+			continue
+		}		
 		if g.state == genbankStateOrigin {
 			trimmed := bytes.Trim(scanner.Bytes(), " 0123456789")
 			buf.Write(trimmed)
+			continue
 		}
 		if g.state == genbankStateFeatures {
 			features.Write(scanner.Bytes())
 			features.Write([]byte("\n"))
+			continue
 		}
 	}
 	return g
